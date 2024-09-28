f8b9c039dba796aad4402d37c8a8f884801ef93d
ihsanican <ihsanmaulana498@gmail.com>
2024-09-28 19:38:11 +0300

    adding shell script to obtain unique genes

---
+++ unique_gene.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+prokka_dir="/home/maulanmi/genome_annotation/prokka_with_genes"
+output="/home/maulanmi/genome_annotation/list_of_genes.txt"
+
+for filename in "$prokka_dir"/*; do
+    file=$(find "$filename" -name "*tsv")
+    cat "$file" | cut -f 4 | grep -v '^$' | uniq >> "$output"
+done
+
+uniq "$output" >> "unique_genes.txt"
